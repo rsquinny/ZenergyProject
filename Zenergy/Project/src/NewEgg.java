1a0d268f3b21b2585e4e4413e55c5262d841922b
rsquinny <ryan.staggers@gmail.com>
2018-04-04 16:46:52 -0400

    Update NewEgg.java

--- Zenergy/Project/src/NewEgg.java
+++ Zenergy/Project/src/NewEgg.java
@@ -9,25 +9,42 @@ public class NewEgg {
 	public static void main(String[] args) throws IOException, InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		//Test Account: "LeatherlikeScaling@maildrop.cc", "Zenergy20"
-		//test.tester();
+				//Test Account: "LeatherlikeScaling@maildrop.cc", "Zenergy20"
 		
-		//TC_Register
+		//TC_Register --- complete
 		/*
 		HomePage home = new HomePage(driver);
 			home.Module_GoToNewegg();
 			home.Module_LoginButton();
 		LoginPage log = new LoginPage(driver);
-			log.Module_Signup("LeatherlikeScaling@maildrop.cc", "Zenergy20");*/
+			log.Module_SignupUserName("LeatherlikeScaling@maildrop.cc");
+			log.Module_SignupPwd("Zenergy20");*/
 			
-		//TC_Search
+		//TC_Search --- complete, could add more searches just to make it special.
+		/*
 		HomePage home = new HomePage(driver);
 			home.Module_GoToNewegg();
 		Searchbar search = new Searchbar(driver);
-			search.Module_ItemSearch("laptop");
+			search.Module_ItemSearch("laptop");*/
 		
-		//TC_PromoCode
-		/*HomePage home = new HomePage(driver);
+		//TC_AddToWishlist --- completed
+		/*
+		HomePage home = new HomePage(driver);
+			home.Module_GoToNewegg();
+			home.Module_LoginButton();
+		LoginPage log = new LoginPage(driver);
+			log.Module_InputLogin("LeatherlikeScaling@maildrop.cc", "Zenergy20");
+			home.Module_GoToNewegg();
+		Searchbar bar = new Searchbar(driver);
+			bar.Module_ItemSearch("PS4 Systems");
+		SearchPage search = new SearchPage(driver);
+			search.Module_ClickItem();
+		ProductPage prod = new ProductPage(driver);
+			prod.Module_AddToWishList();*/
+			
+		//TC_PromoCode --- complete
+		/*
+		HomePage home = new HomePage(driver);
 			home.Module_GoToNewegg();
 		LoginPage log = new LoginPage(driver);
 			log.Module_SignupUserName("LeatherlikeScaling@maildrop.cc");*/
