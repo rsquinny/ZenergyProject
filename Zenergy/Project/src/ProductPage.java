0445a755d207c38f7359a7a4d3b9481da2c5209f
fueyang96 <37555599+fueyang96@users.noreply.github.com>
2018-04-05 11:44:38 -0400

    Update ProductPage.java

--- Zenergy/Project/src/ProductPage.java
+++ Zenergy/Project/src/ProductPage.java
@@ -21,32 +21,54 @@ public class ProductPage {
 				"//img[@title='HP Laptop 250 G6 (1NW57UT#ABA) Intel Core i5 7th Gen 7200U (2.50 GHz) 8 GB Memory 256 GB SSD Intel HD Graphics 620 15.6\" Windows 10 Pro 64-Bit']"));
 		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", laptop);
 		laptop.click();
-
+	
 		// Assigning the cart button to a webelement and clicking
 		WebElement button = driver.findElement(By.cssSelector("button.btn.btn-primary.btn-wide"));
 		button.click();
 		// Waiting for page to load
 		Thread.sleep(2000);
+		//try/catch, If statements to close the popup if displayed
+		try {
+			if (driver.findElement(By.xpath("//html//div[@id='custom']//button[1]")).isDisplayed()) {
+				driver.findElement(By.xpath("//html//div[@id='custom']//button[1]")).click();
+			}
+			Thread.sleep(2000);
+			if (driver.findElement(By.xpath("//div[@class='centerPopup is-current is-top']//div[@class='centerPopup-body']//a[@href='javascript:;']")).isDisplayed()) {
+				driver.findElement(By.xpath("//div[@class='centerPopup is-current is-top']//div[@class='centerPopup-body']//a[@href='javascript:;']")).click();
+			}
+			Thread.sleep(2000);
+		}
+		catch (Exception e) {
+			if (driver.findElement(By.xpath("//html//button[2]")).isDisplayed()) {
+				driver.findElement(By.xpath("//html//button[2]")).click();
+			}
+		}
+			
 
-		// Closes warranty popups
-		driver.findElement(By.xpath("//html//div[@id='custom']//button[1]")).click();
-		Thread.sleep(2000);
-		driver.findElement(By.xpath(
-				"//div[@class='centerPopup is-current is-top']//div[@class='centerPopup-body']//a[@href='javascript:;']"))
-				.click();
-		Thread.sleep(2000);
 	}
 
 	// Module_Checkout: After adding item to your cart, this checksout
 	public void Module_Checkout() throws InterruptedException, IOException, AWTException {
 		// Call modules to open new egg and login, as well as carting and item search
-		// Clicking checkout
-		driver.findElement(By.xpath("//html//button[2]")).click();
+		//Clicking checkout 
+		//try/catch, if statements to checkout
+		try {
+			if (driver.findElement(By.xpath("//html//button[2]")).isDisplayed()) {
+				driver.findElement(By.xpath("//html//button[2]")).click();
+			}
+			if (driver.findElement(By.xpath("//a[@class='button button-primary has-icon-right']")).isDisplayed()) {
+				driver.findElement(By.xpath("//a[@class='button button-primary has-icon-right']")).click();
+			}
+			Thread.sleep(2000);
+		}
+		catch (Exception e) {
+			if (driver.findElement(By.xpath("//a[@class='button button-primary has-icon-right']")).isDisplayed()) {
+				driver.findElement(By.xpath("//a[@class='button button-primary has-icon-right']")).click();
+			}
+		}
 		Thread.sleep(2000);
-		driver.findElement(By.className("button button-primary has-icon-right")).click();
-		Thread.sleep(2000);
-
 	}
+
 
 	// Module_AddToWishlist
 	public void Module_AddToWishList() {
