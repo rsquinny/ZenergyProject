1fb62c9934a329ba01b479b981a50718975df086
fueyang96 <37555599+fueyang96@users.noreply.github.com>
2018-04-05 09:43:21 -0400

    Update NewEgg.java
    figured we could make static drivers so we won't have to create new drivers each time we need to call a module from different classess

--- Zenergy/Project/src/NewEgg.java
+++ Zenergy/Project/src/NewEgg.java
@@ -1,53 +1,81 @@
 import java.io.IOException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.awt.AWTException;
+
 
 
 
 public class NewEgg {
 
+	static WebDriver driver = new ChromeDriver();
+	static HomePage home = new HomePage(driver);
+	static Searchbar bar = new Searchbar(driver);
+	static SearchPage search = new SearchPage(driver);
+	static LoginPage log = new LoginPage(driver);
+	static ProductPage prod = new ProductPage(driver);
+	
 	public static void main(String[] args) throws IOException, InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+	
 				//Test Account: "LeatherlikeScaling@maildrop.cc", "Zenergy20"
 		
 		//TC_Register --- complete
 		/*
-		HomePage home = new HomePage(driver);
 			home.Module_GoToNewegg();
 			home.Module_LoginButton();
-		LoginPage log = new LoginPage(driver);
 			log.Module_SignupUserName("LeatherlikeScaling@maildrop.cc");
 			log.Module_SignupPwd("Zenergy20");*/
 			
 		//TC_Search --- complete, could add more searches just to make it special.
 		/*
-		HomePage home = new HomePage(driver);
 			home.Module_GoToNewegg();
-		Searchbar search = new Searchbar(driver);
 			search.Module_ItemSearch("laptop");*/
 		
 		//TC_AddToWishlist --- completed
 		/*
-		HomePage home = new HomePage(driver);
 			home.Module_GoToNewegg();
 			home.Module_LoginButton();
-		LoginPage log = new LoginPage(driver);
 			log.Module_InputLogin("LeatherlikeScaling@maildrop.cc", "Zenergy20");
 			home.Module_GoToNewegg();
-		Searchbar bar = new Searchbar(driver);
 			bar.Module_ItemSearch("PS4 Systems");
-		SearchPage search = new SearchPage(driver);
 			search.Module_ClickItem();
-		ProductPage prod = new ProductPage(driver);
 			prod.Module_AddToWishList();*/
 			
 		//TC_PromoCode --- complete
 		/*
-		HomePage home = new HomePage(driver);
 			home.Module_GoToNewegg();
-		LoginPage log = new LoginPage(driver);
 			log.Module_SignupUserName("LeatherlikeScaling@maildrop.cc");*/
+		
+		//TC_Compare --- complete
+		/*
+			home.Module_GoToNewegg();
+			bar.Module_ItemSearch("laptop");
+			search.Module_Compare();
+		*/
+		//TC_FeaturedSellers --- complete
+		/*
+			home.Module_GoToNewegg();
+			search.Module_FeaturedSellers();
+		*/
+		//TC_Checkout as user --- complete
+		/*
+			home.Module_GoToNewegg();
+			bar.Module_ItemSearch("laptop");
+			prod.Module_Cart();
+			prod.Module_Checkout();
+			log.Module_InputLogin("LeatherlikeScaling@maildrop.cc", "Zenergy20");
+		*/
+		//TC_Checkout as guest --- complete
+		/*
+			home.Module_GoToNewegg();
+			home.Module_LoginButton();
+			log.Module_SignupUserName("LeatherlikeScaling@maildrop.cc");
+			log.Module_SignupPwd("Zenergy20");
+			bar.Module_ItemSearch("laptop");
+			prod.Module_Cart();
+			prod.Module_Checkout();
+		*/
 	}
 }
 
