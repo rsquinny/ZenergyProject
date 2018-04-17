3a70ea2bf86121cabaeffcf45e3e0ea789b69ed8
fueyang96 <37555599+fueyang96@users.noreply.github.com>
2018-04-16 23:11:26 -0400

    Update SearchPage.java

--- Zenergy/Project/src/SearchPage.java
+++ Zenergy/Project/src/SearchPage.java
@@ -14,28 +14,34 @@ public class SearchPage {
 
 	// Module_Compare
 	public void Module_Compare() throws InterruptedException, IOException {
+		
+		Log.startModule("Selenium_Module_Compare");
+		
 		// called action required for hovering over popup
 		Actions action = new Actions(driver);
 
 		// Scroll into the views of laptops
 		WebElement laptop = driver.findElement(By.xpath(
-				"//img[@title='HP Laptop 250 G6 (1NW57UT#ABA) Intel Core i5 7th Gen 7200U (2.50 GHz) 8 GB Memory 256 GB SSD Intel HD Graphics 620 15.6\" Windows 10 Pro 64-Bit']"));
+				"//div[@class='list-wrap']//div[@class='standard-box-top']"));
 		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", laptop);
-
+		Log.info("Item scrolled into view");
+		
 		// Clicking and comparing two items
 		WebElement compare1 = driver
 				.findElement(By.xpath("//html//div[1]/div[1]/div[2]/div[1]/div[2]/label[1]/span[1]"));
 		WebElement compare2 = driver
 				.findElement(By.xpath("//html//div[2]/div[1]/div[2]/div[1]/div[2]/label[1]/span[1]"));
 		compare1.click();
-		Thread.sleep(2000);
+		Log.info("Compare1 is clicked");
+		Thread.sleep(1500);
 		compare2.click();
-		Thread.sleep(3000);
+		Log.info("Compare2 is clicked");
+		Thread.sleep(1500);
 		WebElement we = driver.findElement(By.xpath("//html//div[2]/div[1]/div[2]/div[1]/div[2]/label[1]/span[1]"));
 		action.moveToElement(we)
 				.moveToElement(driver.findElement(By.xpath("//div[@class='popover-btn-area']/button[@id='gocompare']")))
 				.click().build().perform();
-		Thread.sleep(2500);
+		Log.info("Compare button is clicked");
 
 		// Getting the current title page
 		String actTitle = driver.getTitle();
@@ -44,28 +50,32 @@ public class SearchPage {
 		// verification
 		if (actTitle.equalsIgnoreCase(expTitle)) {
 			System.out.println("Title matched");
+			Log.info("Title matched");
 		} else {
 			System.out.println("Title didn't match");
+			Log.error("Title didn't match");
 		}
+		
+		Log.endModule("Selenium_Module_Compare");
 	}
-	
-	//Module_ClickItem: This clicks the first item on a search page
-	public void Module_ClickItem() {
-		WebElement item = driver.findElement(By.xpath("//*[@id=\"bodyArea\"]/section/div/div/div[2]/div/div/div/div[2]/div[1]/div[2]/div[2]/div[27]/div/a"));
-		item.click();
 
 	// Module_FeaturedSellers
 	public void Module_FeaturedSellers() throws InterruptedException, IOException {
+		
+		Log.startModule("Selenium_Module_FeaturedSellers");
+		
 		// action required for hovering
 		Actions action = new Actions(driver);
 
 		// hovering and clicking a featured seller
 		WebElement featured = driver.findElement(By.xpath("//nav[@class='btn-group-cell menu-box marketplace-nav']"));
 		action.moveToElement(featured).perform();
+		Log.info("Featured element is clicked");
 		Thread.sleep(1000);
 		WebElement seller = driver.findElement(By.xpath(
 				"//img[@src='//images10.newegg.com/Marketing_Place/Seller_logo/Seller_AC8W_ad73bcc8-fce7-4158-87fc-51335b3494c9.gif']"));
 		action.click(seller).build().perform();
+		Log.info("Seller element is clicked");
 
 		// Getting the current title page
 		String actTitle = driver.getTitle();
@@ -75,8 +85,23 @@ public class SearchPage {
 		// verification
 		if (actTitle.equalsIgnoreCase(expTitle)) {
 			System.out.println("Title matched");
+			Log.info("Title matched");
 		} else {
 			System.out.println("Title didn't match");
+			Log.error("Title didn't match");
 		}
+		
+		Log.endModule("Selenium_Module_FeaturedSellers");
+	}
+	//Module_ClickItem: This clicks the first item on a search page
+	public void Module_ClickItem() {
+		
+		Log.startModule("Selenium_Module_ClickItem");
+		
+		WebElement item = driver.findElement(By.xpath("//html[1]/body[1]/div[5]/section[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[2]/div[1]/div[1]/a[1]"));
+		item.click();
+		Log.info("Item is clicked");
+		
+		Log.endModule("Selenium_Module_ClickItem");
 	}
 }
