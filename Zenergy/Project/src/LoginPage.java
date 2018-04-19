2d4a17d35d2bed2cfb66a44b171412135426a815
fueyang96 <37555599+fueyang96@users.noreply.github.com>
2018-04-19 11:17:07 -0400

    Update LoginPage.java

--- Zenergy/Project/src/LoginPage.java
+++ Zenergy/Project/src/LoginPage.java
@@ -14,7 +14,7 @@ public class LoginPage {
 
 	// Module_InputLogin: Logs the user into their pre-existing account.
 	public void Module_InputLogin(String Login, String Password) {
-		
+		//Start logging 
 		Log.startModule("Selenium_Module_InputLogin");
 		
 		WebElement UserNameField = driver.findElement(By.id("UserName"));
@@ -35,63 +35,94 @@ public class LoginPage {
 		
 		Log.endModule("Selenium_Module_InputLogin");
 	}
-		public void Module_Names(String First, String Last) {
+	public void Module_Names(String First, String Last) {
+		
+		Log.startModule("Selenium_Module_Names");
+		
 		// Inputs the name of the user
 		WebElement SignUpButton = driver.findElement(By.xpath("/html/body/div[3]/section/div/div[1]/div/div/span/a"));
 		SignUpButton.click();
+		Log.info("Signup button is clicked");
 		WebElement first = driver.findElement(By.id("firstName"));
 		WebElement last = driver.findElement(By.id("lastName"));
 		first.click();
+		Log.info("First is clicked");
 		first.clear();
+		Log.info("First is cleared");
 		first.sendKeys(First);
+		Log.info("Keys sent to first");
 		last.click();
+		Log.info("Last is clicked");
 		last.clear();
+		Log.info("Last is cleared");
 		last.sendKeys(Last);
+		Log.info("Keys sent to last");
+		
+		Log.endModule("Selenium_Module_Names");
 	}
 
 	// Module_SignupUserName: Finds the LoginName field and inputs the Login
 	// information
 	public void Module_SignupUserName(String Login) throws InterruptedException, NoSuchElementException {
+		
+		Log.startModule("Selenium_Module_SignupUserName");
+		
 		// Tries to see if the promo code box is present. If not tries to login user
 		try {
 			WebElement EmailField = driver.findElement(By.id("LoginName"));
 			EmailField.click();
+			Log.info("EmailField is clicked");
 			EmailField.clear();
+			Log.info("EmailField is cleared");
 			EmailField.sendKeys(Login);
+			Log.info("Keys sent to EmailField");
 			EmailField.sendKeys(Keys.ENTER);
 			// There are two of these classes, it finds the second at index 1 and checks the
 			// text there.
 			if (driver.findElements(By.className("promo-code-title")).get(1).getText()
 					.contains("Thank you for subscribing! Your request will be processed within the next 24 hours.")) {
-				System.out.println("You have been signed up.");
+				Log.info("You have been signed up.");
 			} else {
-				System.out.println("Signup failed.");
+				Log.error("Signup failed.");
 			}
 		} catch (Exception E) {
 			// Adds the email
 			WebElement EmailField = driver.findElement(By.id("emailAddress"));
 			EmailField.click();
+			Log.info("EmailField is clicked");
 			EmailField.clear();
+			Log.info("EmailField is cleared");
 			EmailField.sendKeys(Login);
+			Log.info("Keys sent to EmailField");
 			// Checks to make sure email is an email
 			if (Login.contains("@") && Login.contains(".")) {
-				System.out.println("Email is good.");
+				Log.info("Email is good.");
 			} else {
-				System.out.println("Bad email.");
+				Log.error("Bad email.");
 			}
 		}
+		Log.endModule("Selenium_Module_SignupUserName");
 	}
 
 	// Module_SignupPwd: Registers the users password with their new account
 	public void Module_SignupPwd(String Pwd) throws InterruptedException {
+		
+		Log.startModule("Selenium_Module_SignupPwd");
+		
 		// These find the input elements to login with.
 		WebElement PassworField = driver.findElement(By.id("setPwd"));
 		// Password Creation
 		PassworField.click();
+		Log.info("PassworField is clicked");
 		PassworField.clear();
+		Log.info("PassworField is cleared");
 		PassworField.sendKeys(Pwd);
+		Log.info("Keys sent to PassworField");
 		WebElement CreateAccount = driver.findElement(By.xpath("//button[@id='submitRegistration']"));
 		CreateAccount.click();
+		Log.info("CreateAccount is clicked");
+		
+		Log.endModule("Selenium_Module_SignupPwd");
 	}
 
 }
